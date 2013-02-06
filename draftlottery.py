c9b387c295fdf0eb6b85f774d30ee659143353fc
Roger Wang <r.rj.wang@gmail.com>
2013-02-05 23:34:55 -0600

    Add full terminal output to email text

--- draftlottery.py
+++ draftlottery.py
@@ -9,6 +9,7 @@ class DraftLottery:
     teams = {}
     balls = []
     order = []
+    output = ""
 
     def addteam(self, name=None, num_balls=None):
         """ Add team and number of balls to lottery
@@ -23,45 +24,46 @@ class DraftLottery:
             start = len(self.balls)
             for i in range(start, start+num_balls):
                 self.balls.append(team)
-        print "Lottery balls: " + str(self.balls)
+        self.print_and_append("Lottery balls: " + str(self.balls))
 
     def runlottery(self):
         """ Runs the lottery and outputs each pick
         """
         self.order = []
         for i in range(1, len(self.teams)+1):
-            print "-----------------------------"
-            print "Remaining balls: " + str(self.balls)
+            self.print_and_append("-----------------------------")
+            self.print_and_append("Remaining balls: " + str(self.balls))
             self.pullball(i)
-        print "-----------------------------"
-        print "Lottery is done!  Results:"
+        self.print_and_append("-----------------------------")
+        self.print_and_append("Lottery is done!  Results:")
         for i, team in enumerate(self.order):
-            print "Pick " + str(i+1) + ": " + team
+            self.print_and_append("Pick " + str(i+1) + ": " + team)
 
     def pullball(self, num_pick):
         """ Pulls random ball and removes all other balls of that team
         """
         lower = 0
         upper = len(self.balls)-1
-        print "Drawing pick " + str(num_pick) + ", random number between " + str(lower) + " and " + str(upper)  + "..."
+        self.print_and_append("Drawing pick " + str(num_pick) + ", random number between " + str(lower) + " and " + str(upper)  + "...")
         selection = random.randint(lower, upper)
-        print "Selected: " + str(selection)
+        self.print_and_append("Selected: " + str(selection))
         selected_team = self.balls[selection]
         self.order.append(selected_team)
         selected_team_num_balls = len(filter(lambda a: a == selected_team, self.balls))
-        print "Pick " + str(num_pick)  +" is: " + selected_team + ", with " + str(selected_team_num_balls) + "/" + str(len(self.balls)) + " balls"
-        print "Removing " + selected_team + "'s balls"
+        self.print_and_append("Pick " + str(num_pick)  +" is: " + selected_team + ", with " + str(selected_team_num_balls) + "/" + str(len(self.balls)) + " balls")
+        self.print_and_append("Removing " + selected_team + "'s balls")
         self.balls = filter(lambda a: a != selected_team, self.balls)
+
+    def print_and_append(self, text):
+        print text
+        self.output += "<div>" + text + "</div>"
 
     def email(self, recipients):
         #from http://help.mandrill.com/entries/21746308-Sending-via-SMTP-in-various-programming-languages
-        html = ""
-        for i, team in enumerate(self.order):
-            html += "<div>Pick " + str(i+1) + ": " + team + "</div>"
         msg = MIMEMultipart('alternative')
         msg['Subject'] = "Draft lottery results"
         msg['From'] = FROM_EMAIL
-        body = MIMEText(html, 'html')
+        body = MIMEText(self.output, 'html')
         username = MANDRILL_USERNAME
         password = MANDRILL_PASSWORD
         msg.attach(body)
